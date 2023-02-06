8cdceb2fe4e26fec3a7fcfb6fbc7a374cf5af035
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-06 13:49:07 +0300

    add password confirmation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,10 @@
 class RegistrationsController < ApplicationController
     def create
-      user = User.create!(email: params[:user][:email], password: params[:user][:password])
+      user = User.create!(
+        email: params[:user][:email],
+        password: params[:user][:password]
+        password_confirmation: params[:user][:password_confirmation]
+    )
   
       if user
         session[:user_id] = user.id
