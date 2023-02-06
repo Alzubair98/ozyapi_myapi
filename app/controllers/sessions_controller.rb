e61fbacb524b1bda0a14a03b18b34142128664df
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-06 13:41:40 +0300

    fix bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-    include CurrentUserConcern
+    # include CurrentUserConcern
     def create
         user = User.find_by(email: params[:user][:email])
         .try(:authenticate, params[:user][:password]) # to check the password
@@ -9,29 +9,29 @@ class SessionsController < ApplicationController
             render json: {
             stauts: :created,
             logged_in: true,
-            user:
+            user: user
             }
         else
         render json: { stauts: 401 }
         end
     end
   
-    def logged_in
-      if @current_user
-        render json: {
-          logged_in: true,
-          user: @current_user
-        }
-      else
-        render json: {
-          logged_in: false
-        }
-      end
-    end
+    # def logged_in
+    #   if @current_user
+    #     render json: {
+    #       logged_in: true,
+    #       user: @current_user
+    #     }
+    #   else
+    #     render json: {
+    #       logged_in: false
+    #     }
+    #   end
+    # end
   
-    def logout
-      reset_session
-      render json: { status: 200, logged_out: true }
-    end
+    # def logout
+    #   reset_session
+    #   render json: { status: 200, logged_out: true }
+    # end
   end
   
