40882e9104cb7a72f58377db78a39674b6b09d69
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-06 13:13:37 +0300

    add session

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       resource "*",
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
+        credentials: true #allow us to pass the cookie and headers form the front-end to back end
     end
 
     # allow do
