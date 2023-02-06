0ea8a7f284e099958ec8dfdcffa984a6efc069c8
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-06 14:07:38 +0300

    add concern &logged in & logged out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-    # include CurrentUserConcern
+    include CurrentUserConcern
     def create
         user = User.find_by(email: params[:user][:email])
         .try(:authenticate, params[:user][:password]) # to check the password
@@ -16,22 +16,22 @@ class SessionsController < ApplicationController
         end
     end
   
-    # def logged_in
-    #   if @current_user
-    #     render json: {
-    #       logged_in: true,
-    #       user: @current_user
-    #     }
-    #   else
-    #     render json: {
-    #       logged_in: false
-    #     }
-    #   end
-    # end
+    def logged_in
+      if @current_user
+        render json: {
+          logged_in: true,
+          user: @current_user
+        }
+      else
+        render json: {
+          logged_in: false
+        }
+      end
+    end
   
-    # def logout
-    #   reset_session
-    #   render json: { status: 200, logged_out: true }
-    # end
+    def logout
+      reset_session
+      render json: { status: 200, logged_out: true }
+    end
   end
   
