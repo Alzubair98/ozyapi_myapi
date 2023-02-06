b59ea420eb9b20d0b98a092b60587e0de07a6646
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-06 13:55:06 +0300

    fix bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,8 @@ class RegistrationsController < ApplicationController
     def create
       user = User.create!(
         email: params[:user][:email],
-        password: params[:user][:password]
-        password_confirmation: params[:user][:password_confirmation]
-    )
+        password: params[:user][:password],
+        password_confirmation: params[:user][:password_confirmation])
   
       if user
         session[:user_id] = user.id
@@ -16,5 +15,5 @@ class RegistrationsController < ApplicationController
         render json: { status: 500 }
       end
     end
-  end
+end
   
