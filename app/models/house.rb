a7695b8c980cc67faac19e304a1fe1920b0f1526
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-11 16:47:42 +0300

    add validation to houses

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,6 +2,17 @@ class House < ApplicationRecord
     # has_one_attached :image
     has_many_attached :images
 
+    validates :house_type, presence: true
+    validates :real_price, presence: true
+    validates :price, presence: true
+    validates :location, presence: true
+    validates :owner_number, presence: true
+    validates :age, presence: true
+    validates :size, presence: true
+    validates :rooms, presence: true
+    validates :bath_rooms, presence: true
+    validates :ref_number, presence: true
+
     # def image_url 
     #     Rails.application.routes.url_helpers.url_for(image) if image.attached?
     # end
