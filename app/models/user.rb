7e4dfa7f4ce8e92d8c39cb9c660f8d3621d3f6f5
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-11 16:45:39 +0300

    add more validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
                          length: { minimum: 3, maximum: 90 },
                          uniqueness: { case_sensitive: false }
                          has_secure_password
+    validates :number, presence: true
 end
